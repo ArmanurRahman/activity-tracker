2792edba2f4e91a92ceb9cc4d2ef211e684a3291
Armanur Rahman <mubeenarman19@gmail.com>
2021-11-07 15:27:25 +0900

    implement activity list

--- src/container/Activity/Activity.tsx
+++ src/container/Activity/Activity.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ActivityCard from "../../component/Activity/ActivityCard";
 import AddActivity from "../../component/Activity/AddActivity";
 
 const Activity: React.FC = () => {
@@ -8,8 +9,34 @@ const Activity: React.FC = () => {
         setIsAdd((prevStete) => !prevStete);
     };
     return (
-        <div>
-            <button onClick={addActivityHandler}>Add</button>
+        <div className='w-full h-full flex gap-8 flex-wrap'>
+            <div className='w-56 h-56 bg-white rounded-lg'>
+                <div
+                    className='flex items-center justify-center h-full text-green-600 cursor-pointer'
+                    onClick={addActivityHandler}
+                >
+                    <svg
+                        xmlns='http://www.w3.org/2000/svg'
+                        className='h-6 w-6'
+                        fill='none'
+                        viewBox='0 0 24 24'
+                        stroke='currentColor'
+                    >
+                        <path
+                            strokeLinecap='round'
+                            strokeLinejoin='round'
+                            strokeWidth={2}
+                            d='M12 4v16m8-8H4'
+                        />
+                    </svg>
+                </div>
+            </div>
+            <ActivityCard />
+            <ActivityCard />
+            <ActivityCard />
+            <ActivityCard />
+            <ActivityCard />
+
             {isAdd && <AddActivity />}
         </div>
     );
